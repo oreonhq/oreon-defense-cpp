33e81415d46aa8d719d3583d6b3ddd89b8d20c98
Red <hellx20923@proton.me>
2026-06-06 11:25:52 +1000

    Update README, start integrating ClamAV support.

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Oreon Defense is a frontend to ClamAV, firewalld, nftables, and a few other security-related things.
 
+## Dependencies
+- qt6
+- clamav-devel
+
+## License
 Copyright (C) 2026 Oreon HQ. This program is licensed under GPL-3.0. See [LICENSE](LICENSE).
--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <QtWidgets>
 #include <QtQml>
 
+#include "clamav.h"
+
 #include <iostream>
 #include <filesystem>
+#include <future>
+
+void run_clamav_daemon();
 
 int main(int argc, char** argv) {
 	QApplication app(argc, argv);
 	std::cout << "App initialized" << std::endl;
+
 
 	QQmlApplicationEngine engine; //("ui/Main.qml");
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
@@ -21,5 +27,33 @@ int main(int argc, char** argv) {
 	// }
 	
 	std::cout << "About to execute" << std::endl;
+
+	auto a = std::async(run_clamav_daemon);
+
 	return app.exec();
+}
+
+void run_clamav_daemon() {
+	int success;
+
+	success = cl_init(CL_INIT_DEFAULT);
+	std::cout << (success == CL_SUCCESS ? "Initialized ClamAV" : "Failed to initialize ClamAV!") << std::endl;
+
+	struct cl_engine* clamav;
+	unsigned int sigs = 0;
+
+	if (!(clamav = cl_engine_new())) {
+		std::cout << "Failed to create ClamAV engine!" << std::endl;
+		cl_engine_free(clamav);
+		return;
+	} else {
+		success = cl_load(cl_retdbdir(), clamav, &sigs, CL_DB_STDOPT);
+		if (success != CL_SUCCESS) {
+			std::cout << "Failed to initialize ClamAV DB!" << std::endl;
+			cl_engine_free(clamav);
+			return;
+		} else {
+			std::cout << "Initialized ClamAV DB" << std::endl;
+		}
+	}
 }
